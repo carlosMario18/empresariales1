4214a4a3ea8e6225dc0ecc6164f8e7889bddbb48
C.M Bernal Cuellar <2420201003@estudiantesunibague.edu.co>
2024-02-19 21:54:28 -0500

    Cambiar esto

---
+++ src/model/CitaEspecialista.java
@@ -0,0 +1,41 @@
+package model;
+
+import java.time.LocalDate;
+
+public class CitaEspecialista extends CitaMedica{
+
+    private String especialidad;
+    private int consultorio;
+    private String medico;
+
+    public CitaEspecialista(int id, LocalDate fecha, double costo, String especialidad, int consultorio, String medico) {
+        super(id, fecha, costo);
+        this.especialidad = especialidad;
+        this.consultorio = consultorio;
+        this.medico = medico;
+    }
+
+    public String getEspecialidad() {
+        return especialidad;
+    }
+
+    public void setEspecialidad(String especialidad) {
+        this.especialidad = especialidad;
+    }
+
+    public int getConsultorio() {
+        return consultorio;
+    }
+
+    public void setConsultorio(int consultorio) {
+        this.consultorio = consultorio;
+    }
+
+    public String getMedico() {
+        return medico;
+    }
+
+    public void setMedico(String medico) {
+        this.medico = medico;
+    }
+}
---
+++ src/model/CitaMedica.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.time.LocalDate;
+
+public abstract class CitaMedica {
+
+    private int id;
+    private LocalDate fecha;
+    private double costo;
+
+    public CitaMedica(int id, LocalDate fecha, double costo) {
+        this.id = id;
+        this.fecha = fecha;
+        this.costo = costo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(double costo) {
+        this.costo = costo;
+    }
+
+    @Override
+    public String toString() {
+        return "CitaMedica{" +
+                "id=" + id +
+                ", fecha=" + fecha +
+                ", costo=" + costo +
+                '}';
+    }
+}
