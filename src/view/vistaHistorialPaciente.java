847f2ec5be2cd8167712128bfe4a853097c084cf
Alejandro <alejandrosanmiguel0222@gmail.com>
2024-02-25 17:05:15 -0500

    cambio de nombre en las columnas de las tablas

--- src/view/vistaHistorialPaciente.java
+++ src/view/vistaHistorialPaciente.java
@@ -81,7 +81,7 @@ public class vistaHistorialPaciente extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Nombre", "Fecha", "Tipo de cita", "Motivo"
             }
         ));
         jScrollPane1.setViewportView(tablaHistorialPaciente);
--- src/view/vistaListaPacientes.java
+++ src/view/vistaListaPacientes.java
@@ -76,7 +76,7 @@ public class vistaListaPacientes extends javax.swing.JFrame {
                 {null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                    "N.Identificación", "Nombre", "Fecha", "Tipo de cita", "Motivo"
             }
         ));
         jScrollPane1.setViewportView(tablaListaPacientes);
